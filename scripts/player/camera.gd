07ed24213309d8a2c7bebb6dc3b312ccff2ef97a
cyteon <cyteon>
2024-12-10 20:45:26 +0100

    settings and 3rd person

--- scripts/player/camera.gd
+++ scripts/player/camera.gd
@@ -1,5 +1,9 @@
 extends Camera3D
 
+@onready var third_person_spring: SpringArm3D = $"../../SpringArmNode/SpringArm"
+
+var third_person: bool = false
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
@@ -11,21 +15,28 @@ func _input(event: InputEvent) -> void:
 			elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
 				fov -= 2
 			
-			fov = clamp(fov, 30, 80)
+			fov = clamp(fov, 30, 85)
 	
 	if event is InputEventMouseMotion:
-		get_parent().rotate_y(deg_to_rad(-event.relative.x * .1))
-		
-		rotate_x(deg_to_rad(event.relative.y * .1))
-		
-		if get_parent().spring_length == 10:
-			rotation.x = clamp(rotation.x, deg_to_rad(-20), deg_to_rad(20))
+		if third_person:
+			third_person_spring.get_parent().rotate_y(deg_to_rad(-event.relative.x * .1))
+
+			third_person_spring.rotate_x(deg_to_rad(-event.relative.y * .1))
+			third_person_spring.rotation.x = clamp(third_person_spring.rotation.x, deg_to_rad(-59), deg_to_rad(59))
 		else:
+			get_parent().rotate_y(deg_to_rad(-event.relative.x * .1))
+			
+			rotate_x(deg_to_rad(event.relative.y * .1))
 			rotation.x = clamp(rotation.x, deg_to_rad(-59), deg_to_rad(59))
 	
 	if event.is_action_pressed("switch_perspective"):
-		get_parent().spring_length = 10 if get_parent().spring_length == 0 else 0
-
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_ESCAPE):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		if third_person:
+			third_person = false
+			
+			current = true
+			third_person_spring.get_node("Camera3D").current = false
+		else:
+			third_person = true
+			
+			current = false
+			third_person_spring.get_node("Camera3D").current = true
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,15 +1,8 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var kmh = $AeroBody3D.linear_velocity.length() * 2
-	
-	$CanvasLayer/Control/VBoxContainer/kmh.text = "%s km/h" % int(kmh)
-	$CanvasLayer/Control/VBoxContainer/knots.text = "%s kn" % int(kmh * 0.539957)
-	$CanvasLayer/Control/VBoxContainer/throttle.text = "%s" % int($AeroBody3D.throttle_command * 100) + "%"
+func _process(delta: float) -> void:	
+	if Input.is_action_pressed("pause"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		$CanvasLayer/Control/PauseMenu.visible = true
+		get_tree().paused = true
