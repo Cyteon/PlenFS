9f86f610d34aa8bec208097bf9a04a906c9e6a5c
cyteon <cyteon>
2024-12-16 16:32:16 +0100

    fix btn

--- scripts/menus/main.gd
+++ scripts/menus/main.gd
@@ -4,6 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Version.text = "Version: " + ProjectSettings.get_setting("application/config/version")
+	$OptionButton.selected = Settings.time
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/tutorial.tscn")
